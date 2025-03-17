1f6f67f35eabcf56dbee5724c8e18c61bfe59641
till meeske <till.meeske@gmx.de>
2025-03-17 20:31:44 +0100

    Auto commit: 2025-03-17 20:31:44

--- _pull.sh
+++ _pull.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 cd .
-
 git pull origin master
+$SHELL
--- _push.sh
+++ _push.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-
 cd .
-
 git add .
-
 git commit -m "Auto commit: $(date +'%Y-%m-%d %H:%M:%S')"
-
 git push origin master
+$SHELL
